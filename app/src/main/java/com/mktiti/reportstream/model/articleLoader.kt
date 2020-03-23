bbb8f9cabf554b34f791152fcffcde3bf4c9e2c3
Márkus Krisztián <markuskrisztian1@gmail.com>
2020-03-23 09:58:25 +0100

    Code cleanup

--- app/src/main/java/com/mktiti/reportstream/model/articleLoader.kt
+++ app/src/main/java/com/mktiti/reportstream/model/articleLoader.kt
@@ -1,9 +1,5 @@
 package com.mktiti.reportstream.model
 
-import com.mktiti.reportstream.model.Article
-import com.mktiti.reportstream.model.Language
-import com.mktiti.reportstream.model.LanguageFilter
-
 interface ArticleService {
 
     fun availableLanguages(): List<Language>
